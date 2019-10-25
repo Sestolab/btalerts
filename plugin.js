c133580f89f5201089144f28dcec365f20190b6a
Sestowner <owner@sestolab.pp.ua>
2019-10-25 21:47:39 +0300

    remove alert()

--- plugin.js
+++ plugin.js
@@ -17,12 +17,6 @@ CKEDITOR.plugins.add('btalerts', {
                 dark: 'color: #1b1e21; background-color: #d6d8d9; padding: 1.25rem; box-sizing: border-box;'
             };
 
-        function alert(style, combo){
-            if (combo)
-                return `<p style="${alerts[style]}">${lang[style]}</p>`;
-            return `alert alert-${style}`;
-        }
-
         editor.ui.addRichCombo('btalerts', {
             label: lang.label,
             title: lang.title,
@@ -34,13 +28,13 @@ CKEDITOR.plugins.add('btalerts', {
             init: function(){
                 this.startGroup(lang.panelTitle);
                 for(const item in alerts)
-                    this.add(item, alert(item, 'combo'), lang[item]);
+                    this.add(item, `<p style="${alerts[item]}">${lang[item]}</p>`, lang[item]);
             },
             onClick: function(item){
                 editor.focus();
                 editor.fire('saveSnapshot');
                 if(item != 'normal'){
-                    element.setAttributes({'class': alert(item), 'role': 'alert'});
+                    element.setAttributes({'class': `alert alert-${item}`, 'role': 'alert'});
                     for(const link of element.getElementsByTag('a').$)
                         link.setAttribute('class', 'alert-link');
                 }else if(element.hasClass('alert')){
